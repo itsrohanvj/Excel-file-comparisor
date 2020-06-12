64fb8e75d108969ccf4b6cae7bfb17ff48c2ec15
Rohan Vijay <rohanvj17@live.com>
2020-06-12 22:16:08 +0530

    Update excel files.py

--- excel files.py
+++ excel files.py
@@ -21,18 +21,18 @@ l1=[]
 l2=[]
 try:
     for k  in range(0,a):
-    
+    #here  column is fixed, row is dynamic.
         l1.append(sheet.cell(row=k+1,column=1).value)
     for k in range(0,b):
         l2.append(sheet2.cell(row=k+1,column=1).value)
     print (l1)
     print(l2)
-    for i in range(0,max(a,b)):
-        #for j in range(0, max(a,b)):
+    for i in range(0,min(a,b)):
+        #compares every elements of sheets 1 with all the elements in sheet 2.
         if l1[i] in str(l2):
             continue
         else:
-            print ("absent",l1[i])
+            print ("absent",l1[i]) #if the value is not present in sheet 2, it prints absent and appends that value to another excel sheet called 'final'.
             sheet3.write(count, 0, sheet.cell(row=i,column=1).value)
             wb.save('final.xls')
             count=count+1
